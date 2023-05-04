eefbc5915754bd80bcfba32c56fe469dd51234b7
Onyxcodeotto <110540920+Onyxcodeotto@users.noreply.github.com>
2023-05-04 14:48:33 +0700

    feat: barang

---
+++ src/barang/Barang.java
@@ -0,0 +1,32 @@
+package barang;
+
+import java.io.ObjectInputStream.GetField;
+
+public class Barang {
+    private int idBarang;
+    private String namaBarang;
+    private Double hargaBarang;
+    private Double hargaBeli;
+    private String kategori;
+    private String gambar;
+
+    
+    public int GetIDBarang(){
+        return this.idBarang;
+    }
+    public String GetNamaBarang(){
+        return this.namaBarang;
+    }
+    public Double GetHargaBarang(){
+        return this.hargaBarang;
+    }
+    public Double GetHargaBeli(){
+        return this.hargaBeli;
+    }
+    public String GetKategori(){
+        return this.kategori;
+    }
+    public String GetGambar(){
+        return this.gambar;
+    }
+}
