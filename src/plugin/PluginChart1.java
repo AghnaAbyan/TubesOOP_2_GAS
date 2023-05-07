e742ef9093b75513f3fe2ee30d17696dbcd2616c
manuellaiv <89376200+manuellaiv@users.noreply.github.com>
2023-05-07 17:48:35 +0700

    add plugin

---
+++ src/plugin/PluginChart1.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+import org.jfree.chart.*;
+import org.jfree.data.xy.*;
+
+public class PluginChart1{
+    private XYDataset dataset;
+
+    public PluginChart1(){
+
+    }
+
+    public void createDataset(){
+        var series = new XYSeries("Data penjualan sepanjang waktu");
+        // series.add(<X>, <Y>);
+        // ...
+        var dataset = new XYSeriesCollection();
+        dataset.addSeries(series);
+
+    }
+
+    public void addChartLine(){
+
+    }
+}
