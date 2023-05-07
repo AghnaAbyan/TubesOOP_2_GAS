fa505c143119243293e96ad009d5995e6a664b9e
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-05-07 17:49:31 +0700

    Update Customer.java

--- src/membership/Customer.java
+++ src/membership/Customer.java
@@ -37,7 +37,12 @@ class Member extends Customer {
 class VIP extends Customer {
     private String nama;
     private Double noTelp;
+<<<<<<< Updated upstream
     private int poin;
+=======
+    private double poin;
+    private int hargaBarang;
+>>>>>>> Stashed changes
 
     public String getNama() {
         return this.nama;
@@ -47,16 +52,26 @@ class VIP extends Customer {
         return this.noTelp;
     }
 
-    public int getPoin() {
+    public double getPoin() {
         return this.poin;
     }
 
+<<<<<<< Updated upstream
     public void addPoin(int bayar) {
         this.poin += (0.01 * bayar);
     }
 
     public void minusPoin(int bayar) {
         int amount = 0.1 * bayar;
+=======
+    public void addPoin() {
+        double amount = 0.1 * this.hargaBarang;
+        this.poin += (0.01 * amount);
+    }
+
+    public void minusPoin() {
+        double amount = 0.1 * this.hargaBarang;
+>>>>>>> Stashed changes
         this.poin -= (0.01 * amount);
     }
 }
